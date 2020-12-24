11f2e156b87a31b41a84e21a9ced29efa3f5480c
PanosTsouv <panostsouv95@gmail.com>
2020-12-24 07:48:47 +0200

    Add new classifiers and test them

--- BuildClassifiers.py
+++ BuildClassifiers.py
@@ -1,3 +1,4 @@
+from LogisticRegression import LogisticRegression
 from NaiveBayesImp import NaiveBaseImp
 from TxtDataProcess import TxtDataProcess
 from sklearn.model_selection import train_test_split
@@ -6,18 +7,44 @@ from sklearn.feature_selection import SelectPercentile, f_classif
 from time import time
 
 # path to database as argument ../path/
+print(f'|----------------------------------------------------------------------------------------------------------------|')
+print(f'|----------------------------------------Load Database-----------------------------------------------------------|')
 dataSet = TxtDataProcess(r"D:\University\Artificial_Intelligence/3130212\src\python\enron2\enron2\ham/")
 dataSet.addDataFromTxtFiles()
-dataSet.split_train_test()
-clf = NaiveBaseImp()
-clf.train(dataSet._train_samples_data, dataSet._train_samples_category)
-clf.calculateIG()
-clf.test(dataSet._test_samples_data, dataSet._test_samples_category)
 
+print(f'|----------------------------------------------------------------------------------------------------------------|')
+print(f'|---------------------------Split train data to train-dev-test data----------------------------------------------|')
+dataSet.split_train_dev_test()
+
+print(f'|----------------------------------------------------------------------------------------------------------------|')
+print(f'|---------------------------------Train Naive Bayes Classifier---------------------------------------------------|')
+clf1 = NaiveBaseImp()
+clf1.train(dataSet._train_samples_data, dataSet._train_samples_category)
+
+print(f'|----------------------------------------------------------------------------------------------------------------|')
+print(f'|--------------------------Calculate Information Gaining for attributes------------------------------------------|')
+clf1.calculateIG()
+
+print(f'|----------------------------------------------------------------------------------------------------------------|')
+print(f'|------------------------------Train LogisticRegression Classifier-----------------------------------------------|---AC-----ER-----F1-----RE-----PR---|')
+clf = LogisticRegression()
+clf.create_features([x for x in clf1.ig.keys()][:2000])
+w = clf.train(dataSet._train_samples_data, dataSet._train_samples_category, dataSet)
+
+print(f'|----------------------------------------------------------------------------------------------------------------|')
+print(f'|-------------------------------Test LogisticRegression Classifier-----------------------------------------------|')
+clf.test(dataSet._dev_samples_data, dataSet._dev_samples_category, w, True)
+
+print(f'|----------------------------------------------------------------------------------------------------------------|')
+print(f'|----------------------------------Test Naive Bayes Classifier---------------------------------------------------|')
+clf1.test(dataSet._dev_samples_data, dataSet._dev_samples_category)
+print(f'|----------------------------------------------------------------------------------------------------------------|')
 
 #############################
 # use sklearn lib
-t0 = time()
+print()
+print()
+print(f'|---SKLEARN RESULTS---|')
 features_train, features_test, labels_train, labels_test = train_test_split(dataSet._samplesData, dataSet._samplesCategory, test_size=0.2, random_state=4)
 
 ### text vectorization--go from strings to lists of numbers
@@ -33,12 +60,16 @@ features_train_transformed = selector.transform(features_train_transformed).toar
 features_test_transformed  = selector.transform(features_test_transformed).toarray()
 
 from sklearn.naive_bayes import GaussianNB
+from sklearn import linear_model
 clf = GaussianNB()
 clf.fit(features_train_transformed, labels_train)
-print("training time:", round(time()-t0, 3), "s")
-t1 = time()
 pred = clf.predict(features_test_transformed)
-print("pred time:", round(time()-t1, 3), "s")
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
+
+reg = linear_model.LogisticRegression() 
+reg.fit(features_train_transformed, labels_train)
+y_pred = reg.predict(features_test_transformed)
+accuracy = accuracy_score(y_pred, labels_test)
 print(accuracy)
