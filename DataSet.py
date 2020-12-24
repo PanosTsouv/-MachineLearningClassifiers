8d378a7c0d3ae8a31299313147bd98c1f7d66ea8
PanosTsouv <panostsouv95@gmail.com>
2020-12-24 19:14:25 +0200

    Delete split and helpers methods - Add new split method

--- DataSet.py
+++ DataSet.py
@@ -2,6 +2,8 @@ from random import seed
 import random
 from time import time
 
+import numpy as np
+
 class DataSet:
     def __init__(self, path) -> None:
         self._path = path
@@ -12,6 +14,8 @@ class DataSet:
         self._train_samples_category = []
         self._test_samples_data = []
         self._test_samples_category = []
+        self._dev_samples_data = []
+        self._dev_samples_category = []
 
 
     def dataSetInfo(self):
@@ -19,48 +23,35 @@ class DataSet:
             print(f'Category {category} has {samples} samples')
         print(f'All samples array category : {self._samplesCategory}')
         print(f'Train array category : {self._train_samples_category}')
+        print(f'Dev array category : {self._dev_samples_category}')
         print(f'Test array category : {self._test_samples_category}')
 
+    def split_train_dev_test(self, test_size  = 0, dev_size = 0.2, random_number = 5, percentage_of_sample = 0.8):
+        test_size_hidden = 0
+        if (test_size + dev_size) >= percentage_of_sample: test_size, dev_size = 0.1, 0.1
 
-    def split_train_test(self, test_size  = 0.2, random_number = 1):
+        if percentage_of_sample < 1:
+            test_size_hidden = 10 - percentage_of_sample*10
+            test_size_hidden = test_size_hidden / 10
 
-        t0 = time()
-        # number of test samples per category
-        test_samples_per_category = self._number_of_test_samples_per_cat(test_size)
-        # dir contain all position's samples per category
-        positions_of_samples_per_category = self._positions_of_samples_per_cat()
-        # list contain all position's samples which add in test_set
-        all_selections = self._pick_random_samples(random_number, positions_of_samples_per_category, test_samples_per_category)
-        self._train_samples_category = [sample_cat for sample_cat in self._samplesCategory]
-        self._train_samples_data = [sample_data for sample_data in self._samplesData]
-        for i_sel in all_selections:
-            self._test_samples_data.append(self._samplesData[i_sel])
-            self._test_samples_category.append(self._samplesCategory[i_sel])
-            del self._train_samples_category[i_sel]
-            del self._train_samples_data[i_sel]
-        print("My split time:", round(time()-t0, 3), "s")
+        if test_size != 0: test_size_hidden = test_size
 
+        if random_number != -1: np.random.seed(random_number)
+        
+        temp_array_samples = np.array(self._samplesData)
+        temp_array_samples_cat = np.array(self._samplesCategory)
 
-    def _pick_random_samples(self, random_number, pos_sequence_per_category, test_samples_per_category):
-        seed(random_number)
-        all_selections_pos = []
-        for i_category in self._samplesPerCategory.keys():
-            sequence = pos_sequence_per_category[i_category]
-            selection = random.sample(sequence, test_samples_per_category[i_category])
-            all_selections_pos += selection
-        all_selections_pos = sorted(all_selections_pos, reverse=True)
-        return all_selections_pos
+        for label in self._samplesPerCategory.keys():
+            array_per_label = np.where(np.array(self._samplesCategory) == label)[0]
+            size = array_per_label.size
 
+            sampling_dev = np.random.choice(array_per_label, replace=False, size = (int(size*dev_size) + int(size*test_size_hidden)))
+            array_per_label = array_per_label[~np.isin(array_per_label,sampling_dev)]
 
-    def _number_of_test_samples_per_cat(self, test_size):
-        test_samples_per_category = {}
-        for i_category in self._samplesPerCategory.keys():
-            test_samples_per_category[i_category] = int(self._samplesPerCategory[i_category]*test_size)
-        return test_samples_per_category
-
-
-    def _positions_of_samples_per_cat(self):
-        temp = {}
-        for i_category in self._samplesPerCategory.keys():
-            temp[i_category] = [i_data for i_data in range(len(self._samplesData)) if self._samplesCategory[i_data] == i_category]
-        return temp
+            self._dev_samples_category.extend(temp_array_samples_cat[sampling_dev[0:int(size*dev_size)]])
+            self._dev_samples_data.extend(temp_array_samples[sampling_dev[0:int(size*dev_size)]])
+            if test_size != 0:
+                self._test_samples_category.extend(temp_array_samples_cat[sampling_dev[int(size*dev_size):]])
+                self._test_samples_data.extend(temp_array_samples[sampling_dev[int(size*dev_size):]])
+            self._train_samples_category.extend(temp_array_samples_cat[array_per_label])
+            self._train_samples_data.extend(temp_array_samples[array_per_label])
